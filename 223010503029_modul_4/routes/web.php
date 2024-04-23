2ef4ac988219c6785814d0b7823f9468183ae11b
Luniko Jama <126396293+LunikoJama17@users.noreply.github.com>
2024-04-23 21:09:36 +0700

    modul 4

--- 223010503029_modul_4/routes/web.php
+++ 223010503029_modul_4/routes/web.php
@@ -2,18 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\modul2controller;
 use App\Http\Controllers\headercontroller;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//route resource for products
-Route::resource('/products', \App\Http\Controllers\ProductController::class);
-
-Route::get('/Tugas-mod2', [modul2controller::class, 'index'])->name('modul-2');
-Route::post('login', [modul2controller::class, 'login'])->name('login');
 
 /**
  * route resource posts
